d3de3e6162cdc93a33c8c43ccd05ba58b5bc8f27
Crowdbotics <team@crowdbotics.com>
2020-07-22 19:11:16 +0000

    Update models

--- backend/career_quotient_19106/wsgi.py
+++ backend/career_quotient_19106/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'career_quotient_19106.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "career_quotient_19106.settings")
 
 application = get_wsgi_application()
